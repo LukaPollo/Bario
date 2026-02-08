2409315d9a4d9f44b23ad24145e128c57290aedd
Luka Podiashvili <explorelluka@gmail.com>
2026-02-08 17:19:37 +0100

    Bug fixes
    Fixed getting stuck on the wall & not being able to jump again
    Fixed Spikes not killing the player
    Fixed red blobs going into the wall

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,53 +10,48 @@ public class PlayerController2D : MonoBehaviour
     public AudioClip deathAudio;
     public AudioSource audioSource;
 
+    public Transform groundCheck;
+    public float groundCheckRadius = 0.1f;
+
     private Rigidbody2D rb;
     private bool isGrounded;
-    private int groundContacts;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (!canMove) return;
-        float moveInput = Input.GetAxisRaw("Horizontal");
-        rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
+
+        CheckGround();
 
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
-            isGrounded = false; 
         }
+    }
+
+    void FixedUpdate()
+    {
+        if (!canMove) return;
+
+        float moveInput = Input.GetAxisRaw("Horizontal");
+        rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
+    }
+
+    void CheckGround()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if (((1 << collision.gameObject.layer) & groundLayer) != 0)
-        {
-            groundContacts++;
-            isGrounded = true;
-        }
-        if(collision.gameObject.CompareTag("Enemy") || collision.gameObject.CompareTag("Spike"))
+        if (collision.gameObject.CompareTag("Enemy") || collision.gameObject.CompareTag("Spike"))
         {
             transform.position = spawnPoint.position;
             audioSource.PlayOneShot(deathAudio);
         }
     }
-
-    void OnCollisionExit2D(Collision2D collision)
-    {
-        if (((1 << collision.gameObject.layer) & groundLayer) != 0)
-        {
-            groundContacts--;
-            if (groundContacts <= 0)
-                isGrounded = false;
-        }
-    }
-
-    //animaciok nehezek azokat maskor megcsinaljuk inkabb a projekt masik felehez, de a mozgast megcsinaltam, hogy legyen valami alapunk
 }
